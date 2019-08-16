96dbfb6e6cd51aaa4ca151c30a2d717f4d18be6b
davidh <davidh@iim.csic.es>
2019-08-16 17:01:21 +0100

    A

--- SRC/gen_promoter_cds_problem.m
+++ SRC/gen_promoter_cds_problem.m
@@ -1,4 +1,4 @@
-function model = gen_promoter_cds_problem(promoters,CDSs,RBSs,expData)
+function model = gen_promoter_cds_problem(promoters,CDSs,RBSs,expData,problem_name)
 
 N_PROMOTERS=length(promoters);
 N_CDS=length(CDSs);
@@ -53,7 +53,7 @@ for i=1:length(CDSs)
         if strcmp(codes{j},'protein')
             
             eqn=['d' names{j} '='  translationParameter '*' CDSs{i}] ;
-            states={names{j} CDSs{i}};
+            states={states{:} names{j}};
             eval(names{j});
             parNames={parNames{:} parNamesCDS{:}};
             parValues=[parValues parValuesCDS];
@@ -79,13 +79,67 @@ for i=1:NBINS
     parNames={parNames{:} ['Y' numstr(i)]};
 end
 
-
 eqns=eqns';
 eqns={promEqns{:} eqns{:}};
+states=states';
 
 model.parNames=parNames;
 model.parValues=parValues;
 model.eqns=eqns;
 model.states=states;
+
+%% Basic model definition
+AMIGOModel.n_st =length(states);
+AMIGOModel.n_par = length(parValues);
+AMIGOModel.n_stimulus = 0;
+AMIGOModel.stimulus_names = '';
+AMIGOModel.eqns = char(eqns);
+AMIGOModel.st_names =char(states);
+AMIGOModel.par = parValues;
+AMIGOModel.par_names = char(parNames');
+model_name='PROBLEM';
+
+%% Model execution
+AMIGOModel.names_type='custom';
+AMIGOModel.AMIGOsensrhs=0;
+AMIGOModel.odes_file=[model_name '.c'];
+AMIGOModel.mexfile=[model_name 'CostMex'];
+AMIGOModel.exe_type='costMex';
+AMIGOModel.overwrite_model=1;
+AMIGOModel.compile_model=1;
+AMIGOModel.input_model_type = 'charmodelC';
+AMIGOModel.names_type = 'custom';
+AMIGOModel.cvodes_include=[];
+AMIGOModel.debugmode=0;
+AMIGOModel.shownetwork=0;
+
+iexp=1;
+exps.n_exp=1;
+exps.n_obs{iexp} = 0;
+exps.exp_type{iexp} = 'fixed';
+exps.obs_names{iexp}='';
+exps.obs{iexp}='';
+TIME=0:10:200;
+exps.t_f{iexp} = TIME(end);
+exps.n_s{iexp} = length(TIME);
+exps.t_s{iexp} = TIME;
+
+% exps.u_interp{iexp} = 'step';
+% exps.t_con{iexp} = TIME';
+% exps.n_steps{iexp} = length(TIME)-1;
+% exps.u{iexp} = DATA.data(index,STIMULI_COLS)';
+exps.data_type = 'real';
+exps.noise_type = 'homo';
+exps.exp_data{iexp} = nan(length(TIME),3);
+exps.error_data{iexp} = nan(length(TIME),3);
+exps.exp_y0{iexp} = [0 0 0 0 20 0];
+inputs.PEsol.id_global_theta='none';
+
+inputs.model=AMIGOModel;
+inputs.exps=exps;
+[inputs privstruct]=AMIGO_Prep(inputs);
+
+feval(inputs.model.mexfunction,'sim_CVODES');
+
 
 end
